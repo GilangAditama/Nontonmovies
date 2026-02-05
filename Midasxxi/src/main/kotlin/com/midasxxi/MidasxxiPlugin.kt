98872679e9d7cabd9b741c3f9910ac1254d89a5e
GilangAditama <aditamag84@gmail.com>
2026-02-05 10:58:43 +0700

    Update MidasxxiPlugin.kt

--- Midasxxi/src/main/kotlin/com/midasxxi/MidasxxiPlugin.kt
+++ Midasxxi/src/main/kotlin/com/midasxxi/MidasxxiPlugin.kt
@@ -2,8 +2,6 @@ package com.midasxxi
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import com.lagradost.cloudstream3.plugins.registerMainAPI
-import com.lagradost.cloudstream3.plugins.registerExtractorAPI
 
 @CloudstreamPlugin
 class MidasxxiPlugin : Plugin() {
